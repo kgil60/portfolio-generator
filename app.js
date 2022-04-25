64ddf838200db3709a9c62a0a857b9ab3e940767
Kenny <gilvaryke@gmail.com>
2022-04-25 12:10:29 -0400

    finish site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -137,10 +137,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-            console.log("Portfolio complete")
-        })
-    });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    })
--- src/page-template.js
+++ src/page-template.js
@@ -88,7 +88,7 @@ const generatePage = (templateData) => {
                 <div class="container flex-row justify-space-between align-center py-3">
                     <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
                     <nav class="flex-row">
-                        <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}" target="_blank">Github</a>
+                        <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}" target="_blank">GitHub</a>
                     </nav>
                 </div>
             </header>
